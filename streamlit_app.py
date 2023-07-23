fa04c713338c5d5c2efe94e44e2e5d0c1362788b
Nilesh Sachdeva <76390778+nileshsachdeva@users.noreply.github.com>
2023-07-23 18:33:04 +0530

    Update streamlit_app.py
    chose fruit name column as index

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,11 @@ st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 # put a pick list here so users can pick the fruit they want to include 
-st.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# st.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# picker works, but the numbers don't make any sense! We want the customer to be able to choose the fruits by name
+# choose the Fruit Name Column as the Index
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # display the whole fruits table on the page
 st.dataframe(my_fruit_list)
