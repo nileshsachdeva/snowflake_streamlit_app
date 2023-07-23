59c5159775952d16aaad7ddf01b59b724c444443
Nilesh Sachdeva <76390778+nileshsachdeva@users.noreply.github.com>
2023-07-23 18:34:41 +0530

    Update streamlit_app.py
    uncommented multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,13 +13,12 @@ st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # read fruits csv file
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
-# put a pick list here so users can pick the fruit they want to include 
-# st.multiselect("Pick some fruits:", list(my_fruit_list.index))
-
 # picker works, but the numbers don't make any sense! We want the customer to be able to choose the fruits by name
 # choose the Fruit Name Column as the Index
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# put a pick list here so users can pick the fruit they want to include 
+st.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # display the whole fruits table on the page
 st.dataframe(my_fruit_list)
