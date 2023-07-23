7f6f682dad7df87bd3b05557f8e023d0da21468b
Nilesh Sachdeva <76390778+nileshsachdeva@users.noreply.github.com>
2023-07-23 18:39:35 +0530

    Update streamlit_app.py
    pre-populated fruit picker with ['Avocado', 'Strawberries']

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,8 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # put a pick list here so users can pick the fruit they want to include 
-st.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# filter the table data based on the fruits a customer will choose, so we'll pre-populate the list to set an example for the customer
+st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 
 # display the whole fruits table on the page
 st.dataframe(my_fruit_list)
