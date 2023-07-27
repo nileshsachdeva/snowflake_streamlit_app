69acc43238231f6e5539603c5e03d6595a303b2a
Nilesh Sachdeva <76390778+nileshsachdeva@users.noreply.github.com>
2023-07-27 16:01:52 +0530

    Update streamlit_app.py
    add st.stop() to troubleshoot code

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 st.title("My Parents New Healthy Diner")
 
@@ -49,7 +50,7 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 st.dataframe(fruityvice_normalized)
 
 # don't run anything past here while we troubleshoot
-# st.stop()
+st.stop()
 
 # # query snowflake account metadata
 # my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
